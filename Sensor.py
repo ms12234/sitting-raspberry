2ed28014aad2544bd1b55278a72b1dbbc1401803
s <sm123@protonmail.com>
2017-12-04 17:22:47 +0100

    Added reading button state

--- Sensor.py
+++ Sensor.py
@@ -2,9 +2,9 @@ from position import Position
 
 
 class Sensor:
-    def __init__(self):
+    def __init__(self, position: Position, value: float):
         self.x: float = 0.0
         self.y: float = 0.0
-        self.position: Position = Position.Back
-        self.value: float = 0.0
+        self.position: Position = position
+        self.value: float = value
 
--- main.py
+++ main.py
@@ -7,24 +7,31 @@ from Measurement import Measurement
 from Sensor import Sensor
 from Serializer import Serializer
 from position import Position
+import RPi.GPIO as GPIO
+import time
 
 
 def read_sensors() -> Measurement:
     measurement = Measurement()
-    sensor = Sensor()
-    sensor.position = Position.Back
-    sensor.x = 1
-    sensor.y = 1
-    sensor.value = 1
-    measurement.sensors.append(sensor)
-    measurement.sensors.append(sensor)
-    measurement.sensors.append(sensor)
-    measurement.sensors.append(sensor)
+    for pin in pin_numbers:
+        measurement.sensors.append(Position.Back, get_value_from_sensor(pin))
     return measurement
+
+
+def get_value_from_sensor(pin_number: int):
+    input_pin = GPIO.input(pin_number)
+    if not input_pin:
+        return 1
+    else:
+        return 0
 
 
 serializer = Serializer()
 headers = {'content-type': 'application/json'}
+pin_numbers = [14]
+GPIO.setmode(GPIO.BCM)
+for pin in pin_numbers:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     measurement = read_sensors()
